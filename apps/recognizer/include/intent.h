6daa38a70701fd674c5e2e16467744e000146d6e
medhat_fahmy <medhat.fahmy@harman.com>
2021-04-18 02:53:08 +0200

    Create recognizer class

---
+++ apps/recognizer/include/intent.h
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<map>
+#include<sstream>
+#include<string>
+#define DEBUG       0
+class Recognizer{
+    private:
+    std::map<std::string,int> weather_dtb {{"weather",0},{"today?",0},{"in",0},{"Paris",0},{"Cairo",0},{"New",0},{"York",0}} ;
+    std::map<std::string,int> calender_dtb {{"free",0},{"today?",0},{"tomorrow?",0},{"PM",0},{"AM",0}} ;
+    std::map<std::string,int> fact_ftb {{"fact.",0},{"interesting.",0},{"information.",0},{"Tell",0}} ;
+    public: 
+    Recognizer(std::stringstream &ss);
+    
+};
---
+++ apps/recognizer/src/intent.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<map>
+#include<sstream>
+#include<string>
+#include "intent.h"
+
+    Recognizer::Recognizer(std::stringstream &ss)
+    {
+        int dtb_size=5 ; 
+        int intent_weather=0;
+        int intent_calender=0;
+        int intent_fact=0;
+        std::string it; 
+        while(ss>>it)
+        {
+            if(weather_dtb.contains(it)) // contains C++20
+            {
+                weather_dtb[it]++;
+                intent_weather++;
+            }   
+            if (calender_dtb.contains(it))
+            {
+                calender_dtb[it]++;
+                intent_calender++;
+            }
+            if (fact_ftb.contains(it))
+            {
+                fact_ftb[it]++;
+                intent_fact ++ ;
+            }
+            #if (DEBUG)
+            std::cout<< it<<std::endl ;
+            #endif
+        }
+        if(intent_weather >= 2 )
+        {
+            if((weather_dtb["Paris"] >0) || (weather_dtb["York"] > 0))
+            std::cout<<"Intent: Get Weather City "<< std::endl;
+            else 
+            std::cout<<"Intent: Get Weather"<<std::endl;
+        }
+        else if(intent_calender >=2 )
+        {
+            std::cout<<"Intent: Check calendar"<<std::endl;
+        }
+        else if(intent_fact >=2)
+        {
+            std::cout<<"Intent: Get Fact"<<std::endl ; 
+        }
+
+    }
